f25e7bf9fbde4c8c6c3a9daa60680a283726d4a2
Shaker Kattoula <smkattoula@gmail.com>
2020-08-09 15:04:54 -0700

    create HTML markup, css styling, and rules button functionality

---
+++ script.js
@@ -0,0 +1,12 @@
+const rulesBtn = document.getElementById("rules-btn");
+const closeBtn = document.getElementById("close-btn");
+const rules = document.getElementById("rules");
+
+// Event listeners
+rulesBtn.addEventListener("click", () => {
+  rules.classList.add("show");
+});
+
+closeBtn.addEventListener("click", () => {
+  rules.classList.remove("show");
+});
