3b6c170d7e56ee78d87365d029d4f48cab3d5da5
Shaker Kattoula <smkattoula@gmail.com>
2020-08-09 18:55:36 -0700

    create brickson canvas

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const ctx = canvas.getContext("2d");
 // Init score
 let score = 0;
 
+const brickRowCount = 9;
+const brickColumnCount = 5;
+
 // Create ball properties
 const ball = {
   x: canvas.width / 2,
@@ -26,6 +29,40 @@ const platform = {
   speed: 8,
   dx: 0,
 };
+
+// Create brick properties
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true,
+};
+
+// Create a function that creates the bricks
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColumnCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
+
+// Create a function that draws the bricks on canvas
+function drawBricks() {
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
+      ctx.beginPath();
+      ctx.rect(brick.x, brick.y, brick.w, brick.h);
+      ctx.fillStyle = brick.visible ? "#0095dd" : "transparent";
+      ctx.fill();
+      ctx.closePath();
+    });
+  });
+}
 
 // Create a function that draws a ball on the canvas
 function drawBall() {
@@ -53,6 +90,7 @@ function drawScore() {
 
 // Create a function that draws everything
 function draw() {
+  drawBricks();
   drawBall();
   drawPlatform();
   drawScore();
